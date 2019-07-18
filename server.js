13d28432df56e0e6acd9d966dba12b867e8bfcb6
Minori Hashimoto <minori.fh@gmail.com>
2019-07-18 16:01:23 -0700

    add while statement to only do 20 iterations of each functions

--- server.js
+++ server.js
@@ -2,23 +2,24 @@ var cheerio = require("cheerio");
 var axios = require("axios");
 
 axios.get("https://petsmartcharities.org/adopt-a-pet/find-a-pet?city_or_zip=94105&species=dog&other_pets=_none&form_build_id=form-Q8G91jKYwU43iYKOE9O6DUusJ5_BtUw4P1YDdC7PBfU&form_id=adopt_a_pet_search_block_form&op=Search").then(function(response){
-
     var $ = cheerio.load(response.data);
     var pmResults = [];
 
     $(".aap-item-wrapper").each(function(i, element){
 
-        var name = $(element).find("h4").text()
-        var breed = $(element).find("h6").text()
-        var imgLink = $(element).children(".aap-pet-photo").children("img").attr("src")
-        var moreInfo = $(element).find("a").attr("href");
-
-        pmResults.push({
-            name: name,
-            breed: breed,
-            imgLink: imgLink,
-            moreInfo: moreInfo
-        });
+        while (i < 21){
+            var name = $(element).find("h4").text()
+            var breed = $(element).find("h6").text()
+            var imgLink = $(element).children(".aap-pet-photo").children("img").attr("src")
+            var moreInfo = $(element).find("a").attr("href");
+    
+            pmResults.push({
+                name: name,
+                breed: breed,
+                imgLink: imgLink,
+                moreInfo: moreInfo
+            });
+        }
     });
 
     // console.log(pmResults);
@@ -31,20 +32,22 @@ axios.get("https://www.rocketdogrescue.org/adopt/adoptees/").then(function(respo
 
     $(".dog-list").children("li").each(function(i, element){
 
-        var moreInfo = $(element).find("h3").children("a").attr("href")
-        var name = $(element).find("h3").children("a").text()
-        var breed1 = $(element).find(".dog-details").children(".detail").children("a").text()
-        var breed2 = breed1.replace(/([A-Z])/g, ', $1')
-        var breed3 = breed2.replace(" , ", " ")
-        var breed = breed3.slice(2)
-        var imgLink = $(element).children(".shadow").children("img").attr("src")
-
-        rdResults.push({
-            name: name,
-            breed: breed,
-            imgLink: imgLink,
-            moreInfo: moreInfo
-        });
+        while (i < 21){
+            var moreInfo = $(element).find("h3").children("a").attr("href")
+            var name = $(element).find("h3").children("a").text()
+            var breed1 = $(element).find(".dog-details").children(".detail").children("a").text()
+            var breed2 = breed1.replace(/([A-Z])/g, ', $1')
+            var breed3 = breed2.replace(" , ", " ")
+            var breed = breed3.slice(2)
+            var imgLink = $(element).children(".shadow").children("img").attr("src")
+    
+            rdResults.push({
+                name: name,
+                breed: breed,
+                imgLink: imgLink,
+                moreInfo: moreInfo
+            });
+        }
     });
 
     // console.log(rdResults);
@@ -57,19 +60,21 @@ axios.get("https://adopt.hssvmil.org/search/searchResults.asp?animalType=3%2C16&
 
     $(".result-wrapper").each(function(i, element){
 
-        var moreInfo = "https://adopt.hssvmil.org" + $(element).children(".pic-wrap").children("a").attr("href")
-        var imgLink = "https://adopt.hssvmil.org" + $(element).children(".pic-wrap").children("a").children("img").attr("src")
-        var name = $(element).children(".name-link").children("a").children(".search-result-name-width").children("#test").text()
-        var breed1 = $(element).children(".pic-wrap").children(".hovertext").text().trim().split("                ")
-        var breed = breed1[1]
-
-
-        hsResults.push({
-            name: name,
-            breed: breed,
-            imgLink: imgLink,
-            moreInfo: moreInfo
-        });
+        while (i < 21){
+            var moreInfo = "https://adopt.hssvmil.org" + $(element).children(".pic-wrap").children("a").attr("href")
+            var imgLink = "https://adopt.hssvmil.org" + $(element).children(".pic-wrap").children("a").children("img").attr("src")
+            var name = $(element).children(".name-link").children("a").children(".search-result-name-width").children("#test").text()
+            var breed1 = $(element).children(".pic-wrap").children(".hovertext").text().trim().split("                ")
+            var breed = breed1[1]
+    
+    
+            hsResults.push({
+                name: name,
+                breed: breed,
+                imgLink: imgLink,
+                moreInfo: moreInfo
+            });
+        }
     });
 
     console.log(hsResults);
