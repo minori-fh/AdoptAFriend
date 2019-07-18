ed5cf2384e3891f62a3169a92d832c2b10a97dc3
Minori Hashimoto <minori.fh@gmail.com>
2019-07-17 19:31:13 -0700

    use axios get method to scrape hssvmil website for name, breed, more info, and image link from site

--- server.js
+++ server.js
@@ -1,34 +1,6 @@
 var cheerio = require("cheerio");
 var axios = require("axios");
 
-
-// axios.get("https://www.petfinder.com/search/dogs-for-adoption/us/ca/san-francisco/").then(function(response){
-
-//     var $ = cheerio.load(response.data);
-//     var results = [];
-
-//     $("pfdc-animal-search-results").each(function(i, element){
-
-//         // console.log(element)
-
-//         var test = $(element).closest().children(".petCard_result")
-//         var pet = test.find("a").attr("href")
-//         console.log(pet)
-//         // var moreInfo = $(element).find("a").attr("href")
-//         // var imgLink = $(element).find("img").attr("src")
-
-//         // console.log(moreInfo)
-//         // console.log(imgLink)
-
-//         // results.push({
-//         //     moreInfo: moreInfo,
-//         //     imgLink: imgLink
-//         // });
-//     });
-
-//     // console.log(results);
-// });
-
 axios.get("https://petsmartcharities.org/adopt-a-pet/find-a-pet?city_or_zip=94105&species=dog&other_pets=_none&form_build_id=form-Q8G91jKYwU43iYKOE9O6DUusJ5_BtUw4P1YDdC7PBfU&form_id=adopt_a_pet_search_block_form&op=Search").then(function(response){
 
     var $ = cheerio.load(response.data);
@@ -49,14 +21,13 @@ axios.get("https://petsmartcharities.org/adopt-a-pet/find-a-pet?city_or_zip=9410
         });
     });
 
-    // console.log(pmResults);
+    console.log(pmResults);
 });
 
 axios.get("https://www.rocketdogrescue.org/adopt/adoptees/").then(function(response){
 
     var $ = cheerio.load(response.data);
     var rdResults = [];
-
 
     $(".dog-list").children("li").each(function(i, element){
 
@@ -74,4 +45,28 @@ axios.get("https://www.rocketdogrescue.org/adopt/adoptees/").then(function(respo
     });
 
     // console.log(rdResults);
+});
+
+axios.get("https://adopt.hssvmil.org/search/searchResults.asp?animalType=3%2C16&utm%5Fmedium=adopt%5Fnav&datelostfoundyear=%C2%AEionID%3D%2D1&tpage=1&pagesize=15&s=adoption&searchTypeId=4&sortby=6&statusID=3&submitbtn=Find+Animals&task=view&%3F0%26utm%5Fsource=website&utm%5Fterm=dogs&%5Fga=2%2E3286128%2E1129545607%2E1563410065%2D1435790490%2E1563410065").then(function(response){
+
+    var $ = cheerio.load(response.data);
+    var hsResults = [];
+
+    $(".result-wrapper").each(function(i, element){
+
+        var moreInfo = "https://adopt.hssvmil.org" + $(element).children(".pic-wrap").children("a").attr("href")
+        var imgLink = "https://adopt.hssvmil.org" + $(element).children(".pic-wrap").children("a").children("img").attr("src")
+        var name = $(element).children(".name-link").children("a").children(".search-result-name-width").children("#test").text()
+        var breedSplit = $(element).children(".pic-wrap").children(".hovertext").text().split("Wks")
+        var breed = breedSplit[1]
+
+        hsResults.push({
+            name: name,
+            breed: breed,
+            imgLink: imgLink,
+            moreInfo: moreInfo
+        });
+    });
+
+    // console.log(hsResults);
 });
