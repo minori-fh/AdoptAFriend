e50a996d050703c9fbe752cbd13b0d31cb424867
Minori Hashimoto <minori.fh@gmail.com>
2019-07-18 22:08:10 -0700

    add initial styling for card elements on html

--- public/doge.js
+++ public/doge.js
@@ -3,7 +3,7 @@ $.getJSON("/doges", function(data){
     console.log(data)
     for (var i = 0; data.length; i++){
         $("#doges").append(
-            "<div class='card' style='width: 18rem;'>"
+            "<div class='card'>"
             + "<img src ='" + data[i].imgLink + "' class='card-img-top'>"
             + "<div class='card-body'>"
             + "<h5 class='card-title'>" + data[i].name + "</h5>"
