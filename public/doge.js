f7f2b7cf93b93414ae9b7b897e696a8ca6dd8ef7
Minori Hashimoto <minori.fh@gmail.com>
2019-07-18 21:00:25 -0700

    edit server.js to change results from array to object format

---
+++ public/doge.js
@@ -0,0 +1,15 @@
+$.getJSON("/doges", function(data){
+    console.log(data.length)
+    console.log(data)
+    for (var i = 0; data.length; i++){
+        $("#doges").append(
+            "<div class='card' style='width: 18rem;>"
+            + "<img src ='" + data[i].imgLink + "' class='card-img-top'>"
+            + "<div class='card-body'>"
+            + "<h5 class='card-title'>" + data[i].name + "</h5>"
+            + "<p class='card-text'>" + data[i].breed + "</p>"
+            + "<a href='" + data[i].moreInfo + "' class='btn btn-primary'> Visit Site </a>"
+            + "</div> </div>"
+        )
+    };
+});
