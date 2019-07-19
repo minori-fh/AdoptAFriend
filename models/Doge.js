40f89ab69a9b3cedddaf66389c85df95a778edf8
Minori Hashimoto <minori.fh@gmail.com>
2019-07-18 18:14:28 -0700

    create new DogeSchema and create Doge model to export

---
+++ models/Doge.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+var DogeSchema = new Schema({
+    name: {
+        type: String,
+    },
+    breed: {
+        type: String, 
+    },
+    imgLink: {
+        type: String,
+    },
+    moreInfo: {
+        type: String, 
+    }
+});
+
+var Doge = mongoose.model("Doge", DogeSchema)
+
+module.exports = Doge;
