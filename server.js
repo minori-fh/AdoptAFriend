68e13e579b7657f0528e74eb25df9b0f8093a06c
Minori Hashimoto <minori.fh@gmail.com>
2019-07-18 18:23:43 -0700

    correct result parameters in create functions

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var axios = require("axios");
 var mongoose = require("mongoose");
 
 // Require models
-var models = require("./models")
+var db = require("./models")
 
 var PORT = 8080;
 
@@ -45,10 +45,10 @@ app.get("/", function(req, res){
                 moreInfo: moreInfo
             });
 
-            db.Doge.create(result)
+            db.Doge.create(pmResults)
             .then(function(dbDoge) {
               // View the added result in the console
-              console.log(dbArticle);
+              console.log(dbDoge);
               couter++
             })
             .catch(function(err) {
@@ -61,86 +61,86 @@ app.get("/", function(req, res){
     // console.log(pmResults);
     });
 
-    // axios.get("https://www.rocketdogrescue.org/adopt/adoptees/").then(function(response){
+    axios.get("https://www.rocketdogrescue.org/adopt/adoptees/").then(function(response){
 
-    // var $ = cheerio.load(response.data);
-    // var rdResults = [];
+    var $ = cheerio.load(response.data);
+    var rdResults = [];
 
-    // total = total + $(".dog-list").length
+    total = total + $(".dog-list").length
 
-    // $(".dog-list").children("li").each(function(i, element){
+    $(".dog-list").children("li").each(function(i, element){
 
-    //     while (i < 21){
-    //         var moreInfo = $(element).find("h3").children("a").attr("href")
-    //         var name = $(element).find("h3").children("a").text()
-    //         var breed1 = $(element).find(".dog-details").children(".detail").children("a").text()
-    //         var breed2 = breed1.replace(/([A-Z])/g, ', $1')
-    //         var breed3 = breed2.replace(" , ", " ")
-    //         var breed = breed3.slice(2)
-    //         var imgLink = $(element).children(".shadow").children("img").attr("src")
+        while (i < 21){
+            var moreInfo = $(element).find("h3").children("a").attr("href")
+            var name = $(element).find("h3").children("a").text()
+            var breed1 = $(element).find(".dog-details").children(".detail").children("a").text()
+            var breed2 = breed1.replace(/([A-Z])/g, ', $1')
+            var breed3 = breed2.replace(" , ", " ")
+            var breed = breed3.slice(2)
+            var imgLink = $(element).children(".shadow").children("img").attr("src")
     
-    //         rdResults.push({
-    //             name: name,
-    //             breed: breed,
-    //             imgLink: imgLink,
-    //             moreInfo: moreInfo
-    //         });
+            rdResults.push({
+                name: name,
+                breed: breed,
+                imgLink: imgLink,
+                moreInfo: moreInfo
+            });
 
-    //         db.Doge.create(result)
-    //         .then(function(dbDoge) {
-    //           // View the added result in the console
-    //           console.log(dbArticle);
-    //           couter++
-    //         })
-    //         .catch(function(err) {
-    //           // If an error occurred, log it
-    //           console.log(err);
-    //         });
-    //     }
-    // });
+            db.Doge.create(rdResults)
+            .then(function(dbDoge) {
+              // View the added result in the console
+              console.log(dbDoge);
+              couter++
+            })
+            .catch(function(err) {
+              // If an error occurred, log it
+              console.log(err);
+            });
+        }
+    });
 
-    // // console.log(rdResults);
-    // });
+    // console.log(rdResults);
+    });
 
-    // axios.get("https://adopt.hssvmil.org/search/searchResults.asp?animalType=3%2C16&utm%5Fmedium=adopt%5Fnav&datelostfoundyear=%C2%AEionID%3D%2D1&tpage=1&pagesize=15&s=adoption&searchTypeId=4&sortby=6&statusID=3&submitbtn=Find+Animals&task=view&%3F0%26utm%5Fsource=website&utm%5Fterm=dogs&%5Fga=2%2E3286128%2E1129545607%2E1563410065%2D1435790490%2E1563410065").then(function(response){
+    axios.get("https://adopt.hssvmil.org/search/searchResults.asp?animalType=3%2C16&utm%5Fmedium=adopt%5Fnav&datelostfoundyear=%C2%AEionID%3D%2D1&tpage=1&pagesize=15&s=adoption&searchTypeId=4&sortby=6&statusID=3&submitbtn=Find+Animals&task=view&%3F0%26utm%5Fsource=website&utm%5Fterm=dogs&%5Fga=2%2E3286128%2E1129545607%2E1563410065%2D1435790490%2E1563410065").then(function(response){
 
-    // var $ = cheerio.load(response.data);
-    // var hsResults = [];
+    var $ = cheerio.load(response.data);
+    var hsResults = [];
 
-    // total = total + $(".result-wrapper").length
+    total = total + $(".result-wrapper").length
 
-    // $(".result-wrapper").each(function(i, element){
+    $(".result-wrapper").each(function(i, element){
 
-    //     while (i < 21){
-    //         var moreInfo = "https://adopt.hssvmil.org" + $(element).children(".pic-wrap").children("a").attr("href")
-    //         var imgLink = "https://adopt.hssvmil.org" + $(element).children(".pic-wrap").children("a").children("img").attr("src")
-    //         var name = $(element).children(".name-link").children("a").children(".search-result-name-width").children("#test").text()
-    //         var breed1 = $(element).children(".pic-wrap").children(".hovertext").text().trim().split("                ")
-    //         var breed = breed1[1]
+        while (i < 21){
+            var moreInfo = "https://adopt.hssvmil.org" + $(element).children(".pic-wrap").children("a").attr("href")
+            var imgLink = "https://adopt.hssvmil.org" + $(element).children(".pic-wrap").children("a").children("img").attr("src")
+            var name = $(element).children(".name-link").children("a").children(".search-result-name-width").children("#test").text()
+            var breed1 = $(element).children(".pic-wrap").children(".hovertext").text().trim().split("                ")
+            var breed = breed1[1]
     
     
-    //         hsResults.push({
-    //             name: name,
-    //             breed: breed,
-    //             imgLink: imgLink,
-    //             moreInfo: moreInfo
-    //         });
+            hsResults.push({
+                name: name,
+                breed: breed,
+                imgLink: imgLink,
+                moreInfo: moreInfo
+            });
 
-    //         db.Doge.create(result)
-    //         .then(function(dbDoge) {
-    //           // View the added result in the console
-    //           console.log(dbArticle);
-    //           couter++
-    //         })
-    //         .catch(function(err) {
-    //           // If an error occurred, log it
-    //           console.log(err);
-    //         });
-    //     }
-    // });
+            db.Doge.create(hsResults)
+            .then(function(dbDoge) {
+              // View the added result in the console
+              console.log(dbDoge);
+              couter++
+            })
+            .catch(function(err) {
+              // If an error occurred, log it
+              console.log(err);
+            });
+        }
+    });
 
-    // // console.log(hsResults);
-    // });
+    // console.log(hsResults);
+    });
 
     if(counter === total) {
         res.send("Scrape Complete");
