26f132c733c6f131fd18e6336d867322c680cc69
Minori Hashimoto <minori.fh@gmail.com>
2019-07-20 19:12:24 -0700

    require comment.js model in index.js

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,10 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var CommentSchema = new Schema({
-    body: String,
+    body: {
+        type: String,
+        required: true
+    }
 })
 
 var Comment = mongoose.model("Comment", CommentSchema);
