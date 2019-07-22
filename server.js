d29f46fb19cf9ecafb56f8d040b02c235315e8fe
Minori Hashimoto <minori.fh@gmail.com>
2019-07-22 15:42:08 -0700

    edit mongoose connection

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.static("public"));
 // Connect to MongoDB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/dogedb";
 
-mongoose.connect("mongolab-objective-45747", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
