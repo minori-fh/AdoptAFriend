6fca269dc10e4f8882d9e973a4307ba3586b2305
Minori Hashimoto <minori.fh@gmail.com>
2019-07-27 15:37:35 -0700

    edit conditional to check if there is text in eempty comment div

--- public/doge.js
+++ public/doge.js
@@ -342,7 +342,7 @@ $(document).on("click",".submit", function(){
 
         console.log("emptyAF")
         console.log($(this).siblings(".form-group").children("#comment-empty").length)
-        if($(this).siblings(".form-group").children("#comment-empty").length === 0){
+        if($(this).siblings(".form-group").children("#comment-empty").length === 0 || $(this).siblings(".form-group").children("#comment-empty").text() === ""){
             $(this).siblings(".form-group").append("<p class='switch' id='comment-empty'>Comment cannot be empty!<p>")
             console.log($(this).siblings(".form-group").children("#comment-empty").text())
         } 
