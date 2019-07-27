65794f8897629479a029be1bf1538235c1aa05f1
Minori Hashimoto <minori.fh@gmail.com>
2019-07-27 00:08:33 -0700

    change label to p tag for comment textarea

--- public/doge.js
+++ public/doge.js
@@ -227,7 +227,7 @@ $(document).on("click","#write-comment",function(){
     // Hide the doge display in card and append a textarea for user to write his/her comment
     card.children().hide()
     card.append(
-        "<label for='exampleFormControlTextarea1' class='write-comment-description>Write comment:</label>"
+        "<p class='write-comment-description>Write comment:</p>"
         +"<div class='switch form-group'>"
         +"<textarea class='form-control' id='exampleFormControlTextarea1' rows='6'></textarea>"
         +"</div>"
