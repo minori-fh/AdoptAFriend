05251382702583d389851f830567a84d9ffe29fb
Minori Hashimoto <minori.fh@gmail.com>
2019-07-27 15:40:11 -0700

    edit submit event handler

--- public/doge.js
+++ public/doge.js
@@ -342,6 +342,7 @@ $(document).on("click",".submit", function(){
 
         console.log("emptyAF")
         console.log($(this).siblings(".form-group").children("#comment-empty").length)
+        console.log($(this).siblings(".form-group").children("#comment-empty").text())
         if($(this).siblings(".form-group").children("#comment-empty").length === 0 || $(this).siblings(".form-group").children("#comment-empty").text() === ""){
             $(this).siblings(".form-group").append("<p class='switch' id='comment-empty'>Comment cannot be empty!<p>")
             console.log($(this).siblings(".form-group").children("#comment-empty").text())
