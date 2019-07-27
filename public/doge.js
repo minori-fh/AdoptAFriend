3907af0107fa3ea5281fb53affd99c024a965828
Minori Hashimoto <minori.fh@gmail.com>
2019-07-27 15:35:10 -0700

    use .text method instead of innerHTML

--- public/doge.js
+++ public/doge.js
@@ -344,7 +344,7 @@ $(document).on("click",".submit", function(){
         console.log($(this).siblings(".form-group").children("#comment-empty").length)
         if($(this).siblings(".form-group").children("#comment-empty").length === 0){
             $(this).siblings(".form-group").append("<p class='switch' id='comment-empty'>Comment cannot be empty!<p>")
-            console.log($(this).siblings(".form-group").children("#comment-empty").innerHTML())
+            console.log($(this).siblings(".form-group").children("#comment-empty").text())
         } 
 
     } else {
